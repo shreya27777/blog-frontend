7018ba6163a8c88c32da71a621f014e5ea8bbbe7
shreya sharma <shreyasharmacu@gmail.com>
2019-11-05 13:15:07 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { MyprofileComponent } from './myprofile/myprofile.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    NavbarComponent,
+    FooterComponent,
+    MyprofileComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
